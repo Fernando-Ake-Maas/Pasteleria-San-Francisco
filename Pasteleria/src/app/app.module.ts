0260724e6f82cf0c9fae25351f3e868af62b2cab
Fernando-Ake-Maas <80129774+Fernando-Ake-Maas@users.noreply.github.com>
2022-10-02 13:08:51 -0500

    Add files via upload

---
+++ Pasteleria/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { SucursalesComponent } from './components/sucursales/sucursales.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NosotrosComponent,
+    ProductosComponent,
+    SucursalesComponent,
+    ContactoComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Pasteleria/src/app/components/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  public page_title: string;
+
+  constructor() {
+    
+    this.page_title= "Registro";
+   }
+
+  ngOnInit(): void {
+  }
+
+}
